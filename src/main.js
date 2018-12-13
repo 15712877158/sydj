c19d882666c1ab4b4813368e5e20d2046c62e13e
索嗣尧 <15712877158@163.com>
2018-12-13 23:16:14 +0800

    整体结构布局

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/css/public.css'
+import './assets/css/common.css'
 Vue.config.productionTip = false
-
+Vue.use(Element)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/views/layout.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="layout">
+    <el-container>
+        <el-aside>
+            <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            router>
+                <el-submenu index="1">
+                    <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>导航一</span>
+                    </template>
+                    <el-menu-item index="/table">选项1</el-menu-item>
+                    <el-menu-item index="/echarts">选项2</el-menu-item>
+                    <el-menu-item index="1-3">选项3</el-menu-item>
+                </el-submenu>
+                <el-menu-item index="2">
+                    <i class="el-icon-menu"></i>
+                    <span slot="title">导航二</span>
+                </el-menu-item>
+                <el-menu-item index="3">
+                    <i class="el-icon-document"></i>
+                    <span slot="title">导航三</span>
+                </el-menu-item>
+                <el-menu-item index="4">
+                    <i class="el-icon-setting"></i>
+                    <span slot="title">导航四</span>
+                </el-menu-item>
+            </el-menu>
+        </el-aside>
+        <el-container>
+            <el-header></el-header>
+            <el-main>
+                <router-view/>
+            </el-main>
+        </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+        
+        }
+    },
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .layout{
+        width:100%;
+        height:100%;
+    }
+</style>
