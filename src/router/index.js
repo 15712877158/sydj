bb28633955ac9b96aedce582d6a0120ab4379852
索嗣尧 <15712877158@163.com>
2018-12-14 22:44:13 +0800

    测试node+mongodb+vue登陆注册

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import layout from '@/views/layout'
-import table from '@/views/template/group1/table'
-import echarts from '@/views/template/group1/echarts'
+// import layout from '@/views/layout'
+import login from '@/views/login'
+// import table from '@/views/template/group1/table'
+// import echarts from '@/views/template/group1/echarts'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'layout',
-      component: layout,
-      children:[
-        {
-          path: "/table",
-          name: 'table',
-          component: table,
-        },
-        {
-          path: "/echarts",
-          name: 'echarts',
-          component: echarts,
-        }
-      ]      
+      name: 'login',
+      component: login,
+      // children:[
+      //   {
+      //     path: "/table",
+      //     name: 'table',
+      //     component: table,
+      //   },
+      //   {
+      //     path: "/echarts",
+      //     name: 'echarts',
+      //     component: echarts,
+      //   }
+      // ]      
     }
   ]
 })
